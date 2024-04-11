95338e7f40957c3c7916c6d4948d6b620020346f
FalsePattern <me@falsepattern.com>
2024-04-12 00:49:19 +0200

    gotta go fast

--- src/main/java/team/chisel/client/render/InterpolatedIcon.java
+++ src/main/java/team/chisel/client/render/InterpolatedIcon.java
@@ -12,17 +12,25 @@ package team.chisel.client.render;
 
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.client.resources.SimpleResource;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import lombok.val;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
@@ -44,9 +52,23 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 
 	@Override
 	public void updateAnimation() {
-		super.updateAnimation();
+		if (init != null) {
+			if (!init.isDone()) {
+				super.updateAnimation();
+				return;
+			}
+			init = null;
+		}
+		++this.tickCounter;
+		if (this.tickCounter >= this.animationMetadata.getFrameTimeSingle(this.frameCounter)) {
+			int j = this.animationMetadata.getFrameCount() == 0 ? this.framesTextureData.size() : this.animationMetadata.getFrameCount();
+			this.frameCounter = (this.frameCounter + 1) % j;
+			this.tickCounter = 0;
+		}
 		try {
-			updateAnimationInterpolated(true);
+			val frame = generateFrame(frameCounter, tickCounter);
+			if (frame != null)
+				TextureUtil.uploadTextureMipmap(frame, width, height, originX, originY, false, false);
 		} catch (Exception e) {
 			// NO-OP
 		}
@@ -60,43 +82,9 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 	}
 
 	/**
-	 * Generates an interpolated frame from the current tick and frame counters, and optionally uploads it to the gpu.
-	 * @param upload True if the generated texture should be uploaded. False if the method is being used just for pre-generation.
-	 */
-	private void updateAnimationInterpolated(boolean upload) throws IllegalArgumentException {
-
-		int subFrameCount = animationMetadata.getFrameTimeSingle(frameCounter);
-		int firstFrameIndex = animationMetadata.getFrameIndex(frameCounter);
-		int totalFrameCount = animationMetadata.getFrameCount() == 0 ? framesTextureData.size() : animationMetadata.getFrameCount();
-		int secondFrameIndex = animationMetadata.getFrameIndex((frameCounter + 1) % totalFrameCount);
-
-		double alpha = 1.0D - tickCounter / (double) subFrameCount;
-
-		if (interpolatedFrameData == null || interpolatedFrameData.length != totalFrameCount) {
-			interpolatedFrameData = new int[totalFrameCount][][][];
-		}
-
-		if (firstFrameIndex != secondFrameIndex && secondFrameIndex >= 0 && secondFrameIndex < framesTextureData.size()) {
-			int[][][] subFrames = interpolatedFrameData[firstFrameIndex];
-			if (subFrames == null || subFrames.length != subFrameCount) {
-				interpolatedFrameData[firstFrameIndex] = subFrames = new int[subFrameCount][][];
-			}
-			int[][] subFrame = subFrames[tickCounter];
-			if (subFrame == null) {
-				int[][] firstFrame = (int[][]) framesTextureData.get(firstFrameIndex);
-				int[][] secondFrame = (int[][]) framesTextureData.get(secondFrameIndex);
-				subFrame = subFrames[tickCounter] = interpolate(firstFrame, secondFrame, alpha, null);
-			}
-
-			if (upload)
-				TextureUtil.uploadTextureMipmap(subFrame, width, height, originX, originY, false, false);
-		}
-	}
-
-	/**
 	 * Linearly interpolates between two animation frames, and stores the result in the output. If the output has an incorrect size, or is null, a new array is returned.
 	 */
-	private int[][] interpolate(int[][] first, int[][] second, double alpha, int[][] output) {
+	private static int[][] interpolate(int[][] first, int[][] second, float alpha, int[][] output) {
 		if (output == null || output.length != first.length)
 			output = new int[first.length][];
 
@@ -108,7 +96,7 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 
 			firstIMG = first[mipMapIndex];
 			length = firstIMG.length;
-			if ((outputIMG = output[mipMapIndex]) == null)
+			if ((outputIMG = output[mipMapIndex]) == null || outputIMG.length != length)
 				outputIMG = output[mipMapIndex] = new int[length];
 
 			if (mipMapIndex < second.length && (secondIMG = second[mipMapIndex]).length == length) {
@@ -128,36 +116,142 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 	/**
 	 * Linearly interpolates between RGB colors.
 	 */
-	private int interpolatePixel(int first, int second, double alpha) {
+	private static int interpolatePixel(int first, int second, float alpha) {
 		int r = interpolate((first & 0x00FF0000) >>> 16, (second & 0x00FF0000) >>> 16, alpha);
 		int g = interpolate((first & 0x0000FF00) >>> 8, (second & 0x0000FF00) >>> 8, alpha);
 		int b = interpolate(first & 0x000000FF, second & 0x000000FF, alpha);
 		return first & 0xFF000000 | r << 16 | g << 8 | b;
 	}
 
+	private static final int GAMMA_RES = 2048;
+	private static final float[] UNGAMMA_TABLE = new float[GAMMA_RES + 1];
+	private static final float[] REGAMMA_TABLE = new float[GAMMA_RES + 1];
 
-	private int interpolate(int a, int b, double alpha) {
-		return (int) Math.round(255d * Math.pow(Math.pow(a / 255d, GAMMA) * alpha + Math.pow(b / 255d, GAMMA) * (1d - alpha), 1d / GAMMA));
+	static {
+		for (int i = 0; i < GAMMA_RES + 1; i++) {
+			double unit = i / (double)GAMMA_RES;
+			UNGAMMA_TABLE[i] = (float) Math.pow(unit, GAMMA);
+			REGAMMA_TABLE[i] = (float) Math.pow(unit, 1d / GAMMA);
+		}
+	}
+
+	private static float ungamma(float value) {
+		int index = Math.min(GAMMA_RES, Math.max(0, (int) (value * GAMMA_RES)));
+		return UNGAMMA_TABLE[index];
+	}
+	private static float regamma(float value) {
+		int index = Math.min(GAMMA_RES, Math.max(0, (int) (value * GAMMA_RES)));
+		return REGAMMA_TABLE[index];
+	}
+
+
+	private static int interpolate(int a, int b, float alpha) {
+		return Math.round(255f * regamma(ungamma(a / 255f) * alpha + ungamma(b / 255f) * (1f - alpha)));
+	}
+
+	private static final Semaphore MUTEX = new Semaphore(1);
+	private static final BlockingQueue<FutureTask<?>> tasks = new LinkedBlockingQueue<>();
+	private static Thread workerThread;
+
+	private static Future<?> submit(Runnable task) {
+		val future = new FutureTask<>(task, null);
+		MUTEX.acquireUninterruptibly();
+		tasks.add(future);
+		if (workerThread == null) {
+			workerThread = new Thread(() -> {
+				boolean acquired = false;
+				long lastWorkTime = System.currentTimeMillis();
+				try {
+					MUTEX.acquire();
+					acquired = true;
+					while (!tasks.isEmpty() || System.currentTimeMillis() - lastWorkTime < 500) {
+						MUTEX.release();
+						acquired = false;
+						FutureTask<?> task1;
+						task1 = tasks.poll(500, TimeUnit.MILLISECONDS);
+						if (task1 == null)
+							continue;
+
+						task1.run();
+						lastWorkTime = System.currentTimeMillis();
+						MUTEX.acquire();
+						acquired = true;
+					}
+				} catch (InterruptedException ignored) {
+
+				} finally {
+					workerThread = null;
+					if (acquired)
+						MUTEX.release();
+				}
+			});
+			workerThread.start();
+		}
+		MUTEX.release();
+		return future;
 	}
 
 	/**
 	 * Nukes the frame cache and regenerates it from scratch.
 	 */
 	private void regenerateInterpolationBuffer() {
-		interpolatedFrameData = null;
-		int tickCounterBak = tickCounter;
-		int frameCounterBak = frameCounter;
+		init = submit(() -> {
+			interpolatedFrameData = null;
+			generateFrame(-1, -1);
+		});
+	}
 
+	private Future<?> init;
+
+	private int[][] generateFrame(int frameIndex, int tickIndex) {
 		int frameCount = animationMetadata.getFrameCount() == 0 ? framesTextureData.size() : animationMetadata.getFrameCount();
-		for (frameCounter = 0; frameCounter < frameCount; frameCounter++) {
-			int subFrameCount = animationMetadata.getFrameTimeSingle(frameCounter);
-			for (tickCounter = 0; tickCounter < subFrameCount; tickCounter++) {
-				updateAnimationInterpolated(false);
-			}
-		}
 
-		tickCounter = tickCounterBak;
-		frameCounter = frameCounterBak;
+		if (interpolatedFrameData == null || interpolatedFrameData.length != frameCount) {
+			interpolatedFrameData = new int[frameCount][][][];
+		}
+		if (frameIndex == -1) {
+			for (frameIndex = 0; frameIndex < frameCount; frameIndex++) {
+				generateFrame0(frameIndex, frameCount, tickIndex);
+			}
+			return null;
+		} else {
+			return generateFrame0(frameIndex, frameCount, tickIndex);
+		}
+	}
+
+	private int[][] generateFrame0(int frameIndex, int frameCount, int tickIndex) {
+		int firstFrameIndex = animationMetadata.getFrameIndex(frameIndex);
+		int secondFrameIndex = animationMetadata.getFrameIndex((frameIndex + 1) % frameCount);
+		if (firstFrameIndex != secondFrameIndex && secondFrameIndex >= 0 && secondFrameIndex < framesTextureData.size()) {
+			return generateSubFrame(frameIndex, firstFrameIndex, secondFrameIndex, tickIndex);
+		}
+		return null;
+	}
+
+	private int[][] generateSubFrame(int frameIndex, int firstFrameIndex, int secondFrameIndex, int tickIndex) {
+		int subFrameCount = animationMetadata.getFrameTimeSingle(frameCounter);
+		int[][][] subFrames = interpolatedFrameData[frameIndex];
+		if (subFrames == null)
+			interpolatedFrameData[frameIndex] = subFrames = new int[subFrameCount][][];
+		if (tickIndex == -1) {
+			for (tickIndex = 0; tickIndex < subFrameCount; tickIndex++) {
+				generateSubFrame0(subFrames, tickIndex, subFrameCount, firstFrameIndex, secondFrameIndex);
+			}
+			return null;
+		} else {
+			return generateSubFrame0(subFrames, tickIndex, subFrameCount, firstFrameIndex, secondFrameIndex);
+		}
+	}
+
+	private int[][] generateSubFrame0(int[][][] subFrames, int tickIndex, int subFrameCount, int firstFrameIndex, int secondFrameIndex) {
+		int[][] subFrame = subFrames[tickIndex];
+		if (subFrame == null) {
+			float alpha = 1.0f - tickIndex / (float) subFrameCount;
+			int[][] firstFrame = (int[][]) framesTextureData.get(firstFrameIndex);
+			int[][] secondFrame = (int[][]) framesTextureData.get(secondFrameIndex);
+			subFrame = subFrames[tickIndex] = interpolate(firstFrame, secondFrame, alpha, null);
+		}
+		return subFrame;
 	}
 
 	public static class RegistrationHandler {
