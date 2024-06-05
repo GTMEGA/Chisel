51581a24ab956b2f813d8500b739afb456ab6a81
FalsePattern <me@falsepattern.com>
2024-06-06 01:39:03 +0200

    disable icon interpolation for now

--- src/main/java/team/chisel/client/render/InterpolatedIcon.java
+++ src/main/java/team/chisel/client/render/InterpolatedIcon.java
@@ -39,10 +39,6 @@ import cpw.mods.fml.relauncher.SideOnly;
 // This is all vanilla code from 1.8, thanks to ganymedes01 porting it to 1.7 :D
 @SideOnly(Side.CLIENT)
 public class InterpolatedIcon extends TextureAtlasSprite {
-	/**
-	 * This is really just a GpuTexture[][], but minecraft uses int[][] as gpu textures
-	 */
-	private int[][][][] interpolatedFrameData;
 	private TextureAtlasSprite wrapped;
 
 	public InterpolatedIcon(String name, TextureAtlasSprite wrapped) {
@@ -52,13 +48,6 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 
 	@Override
 	public void updateAnimation() {
-		if (init != null) {
-			if (!init.isDone()) {
-				super.updateAnimation();
-				return;
-			}
-			init = null;
-		}
 		++this.tickCounter;
 		if (this.tickCounter >= this.animationMetadata.getFrameTimeSingle(this.frameCounter)) {
 			int j = this.animationMetadata.getFrameCount() == 0 ? this.framesTextureData.size() : this.animationMetadata.getFrameCount();
@@ -78,7 +67,6 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 	public void initSprite(int p_110971_1_, int p_110971_2_, int p_110971_3_, int p_110971_4_, boolean p_110971_5_) {
 		super.initSprite(p_110971_1_, p_110971_2_, p_110971_3_, p_110971_4_, p_110971_5_);
 		wrapped.copyFrom(this);
-		regenerateInterpolationBuffer();
 	}
 
 	/**
@@ -191,35 +179,8 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 		return future;
 	}
 
-	/**
-	 * Nukes the frame cache and regenerates it from scratch.
-	 */
-	private void regenerateInterpolationBuffer() {
-		init = submit(() -> {
-			interpolatedFrameData = null;
-			generateFrame(-1, -1);
-		});
-	}
-
-	private Future<?> init;
-
 	private int[][] generateFrame(int frameIndex, int tickIndex) {
 		int frameCount = animationMetadata.getFrameCount() == 0 ? framesTextureData.size() : animationMetadata.getFrameCount();
-
-		if (interpolatedFrameData == null || interpolatedFrameData.length != frameCount) {
-			interpolatedFrameData = new int[frameCount][][][];
-		}
-		if (frameIndex == -1) {
-			for (frameIndex = 0; frameIndex < frameCount; frameIndex++) {
-				generateFrame0(frameIndex, frameCount, tickIndex);
-			}
-			return null;
-		} else {
-			return generateFrame0(frameIndex, frameCount, tickIndex);
-		}
-	}
-
-	private int[][] generateFrame0(int frameIndex, int frameCount, int tickIndex) {
 		int firstFrameIndex = animationMetadata.getFrameIndex(frameIndex);
 		int secondFrameIndex = animationMetadata.getFrameIndex((frameIndex + 1) % frameCount);
 		if (firstFrameIndex != secondFrameIndex && secondFrameIndex >= 0 && secondFrameIndex < framesTextureData.size()) {
@@ -228,30 +189,9 @@ public class InterpolatedIcon extends TextureAtlasSprite {
 		return null;
 	}
 
+	//TODO reimplement interpolation without memory pressure
 	private int[][] generateSubFrame(int frameIndex, int firstFrameIndex, int secondFrameIndex, int tickIndex) {
-		int subFrameCount = animationMetadata.getFrameTimeSingle(frameCounter);
-		int[][][] subFrames = interpolatedFrameData[frameIndex];
-		if (subFrames == null)
-			interpolatedFrameData[frameIndex] = subFrames = new int[subFrameCount][][];
-		if (tickIndex == -1) {
-			for (tickIndex = 0; tickIndex < subFrameCount; tickIndex++) {
-				generateSubFrame0(subFrames, tickIndex, subFrameCount, firstFrameIndex, secondFrameIndex);
-			}
-			return null;
-		} else {
-			return generateSubFrame0(subFrames, tickIndex, subFrameCount, firstFrameIndex, secondFrameIndex);
-		}
-	}
-
-	private int[][] generateSubFrame0(int[][][] subFrames, int tickIndex, int subFrameCount, int firstFrameIndex, int secondFrameIndex) {
-		int[][] subFrame = subFrames[tickIndex];
-		if (subFrame == null) {
-			float alpha = 1.0f - tickIndex / (float) subFrameCount;
-			int[][] firstFrame = (int[][]) framesTextureData.get(firstFrameIndex);
-			int[][] secondFrame = (int[][]) framesTextureData.get(secondFrameIndex);
-			subFrame = subFrames[tickIndex] = interpolate(firstFrame, secondFrame, alpha, null);
-		}
-		return subFrame;
+        return (int[][]) framesTextureData.get(firstFrameIndex);
 	}
 
 	public static class RegistrationHandler {
